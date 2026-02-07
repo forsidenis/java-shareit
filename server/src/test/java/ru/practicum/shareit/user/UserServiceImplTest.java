57fca7d8cf5a25c77bfb2479654bdea47da54abe
forsidenis <forsidenis@yandex.ru>
2026-02-08 00:41:49 +0300

    Проект ShareIt. Техническое задание. Часть 3 (выполнено)

--- server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -11,7 +11,6 @@ import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
